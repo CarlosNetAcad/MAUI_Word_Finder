bf2884903f1670444f8e86bc5ad41078bc528966
Carlos Martin Gomez May <carlosmg.may@icloud.com>
2024-11-03 15:44:15 -0500

    Add project files.

---
+++ MAUI.Word.Finder/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI.Word.Finder
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
