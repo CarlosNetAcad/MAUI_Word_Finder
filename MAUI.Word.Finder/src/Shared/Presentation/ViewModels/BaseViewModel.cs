38dabb4cc22ee83c32c83d9047c4dd88534c0786
Carlos Martin Gomez May <carlosmg.may@icloud.com>
2024-11-04 00:31:28 -0500

    Create --> The view model and a parent class.
    The base view model help us to implement the community toolkit in all derived class. It helps us to save coding time.

---
+++ MAUI.Word.Finder/src/Shared/Presentation/ViewModels/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MAUI.Word.Finder.src.Shared.Presentation.ViewModels;
+
+public partial class BaseViewModel : ObservableObject
+{
+    #region Flds
+
+    /// <summary>
+    /// State to set the changes of the properties.
+    /// </summary>
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    /// <summary>
+    /// Title of the UI interface.
+    /// </summary>
+    [ObservableProperty]
+    string title;
+
+    #endregion Fdls
+
+    #region Ctors
+    #endregion
+
+    #region Props
+
+    /// <summary>
+    /// State to set the oposite of isBusy.
+    /// </summary>
+    public bool IsNotBusy => !IsBusy;
+
+    #endregion Props
+}
---
+++ MAUI.Word.Finder/src/WordFinder/Presentation/ViewModels/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MAUI.Word.Finder.src.Shared.Presentation.ViewModels;
+using MAUI.Word.Finder.src.WordFinder.Domain.Services;
+using MAUI.Word.Finder.src.WordFinder.Infrastructure.Repository.MockRepository;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace MAUI.Word.Finder.src.WordFinder.Presentation.ViewModels;
+
+public partial class MainViewModel : BaseViewModel
+{
+    private IWordFinderService _wordFinderService;
+
+    [ObservableProperty]
+    ObservableCollection<string> _wordsFound;
+
+    public MainViewModel()
+    {
+        var matrix = new List<string>
+        {
+            "abcdc",
+            "fgwio",
+            "chill",
+            "pqnsd",
+            "uvdxy"
+        };
+
+        _wordFinderService = new WordFinderService(matrix);
+
+        Title = "Word Finder";
+    }
+
+    [RelayCommand]
+    async Task FindWords()
+    {
+        if (IsBusy) return;
+
+        try
+        {
+            IsBusy = true;
+
+            var wordStream = new List<string>
+            {
+                "cold",
+                "chill",
+                "snow",
+                "wind"
+            };
+
+            var wordsFound = await _wordFinderService.FindAsync(wordStream);
+
+            if (wordsFound.Any())
+            {
+                WordsFound?.Clear();
+
+                foreach (var word in wordsFound)
+                    WordsFound?.Add(word);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+
+            await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+}
