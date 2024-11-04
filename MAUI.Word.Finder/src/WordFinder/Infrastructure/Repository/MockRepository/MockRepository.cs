ade46d569c32c3a48bd5bd73d143a04461ec939e
Carlos Martin Gomez May <carlosmg.may@icloud.com>
2024-11-04 00:32:34 -0500

    Create --> MockRepository
    I want to create an example how to handle repositories, but this is out of the acceptance criteria.

---
+++ MAUI.Word.Finder/src/WordFinder/Infrastructure/Repository/MockRepository/MockRepository.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MAUI.Word.Finder.src.WordFinder.Infrastructure.Repository.MockRepository;
+
+/// <summary>
+/// Matrix wrapper.
+/// </summary>
+/// <param name="rows"></param>
+public record MatrixRecord(List<string> rows);
+
+/// <summary>
+/// WordStream wrapper.
+/// </summary>
+/// <param name="wordStream"></param>
+public record WordStreamRecord(List<string> wordStream);
